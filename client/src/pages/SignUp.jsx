e585d366edc35b0d74e2f61d10022e5a72340283
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-08-24 12:32:15 +0530

    'tst'

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,13 +5,41 @@ export default function SignUp() {
 
   const [formData , setFormdata] = useState({});
   const [loading , setLoading] = useState(false);
+  const [error , setError] = useState(false);
   const handlechange = (e)=>{
       setFormdata({...formData , [e.target.id]:e.target.value });
   }
   
   const handlesubmit = async(e)=>{
     e.preventDefault();
-    const  res = await fetch ("http://")
+
+    try {
+      setLoading(true)
+      setError(false)
+
+      const  res = await fetch("/api/auth/signup", {
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);  {msg : 'user created sucesfully'}
+      if(data.success === false){
+        setError(true)
+        return;
+      }
+
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }finally{
+      setLoading(false)
+    }
+
+
+
   }
 
   return (
@@ -21,14 +49,15 @@ export default function SignUp() {
         <input type="text" placeholder='username' id='username' className='p-3 rounded-lg' onChange={handlechange}/>
         <input type="email" placeholder='email' id='email' className='p-3 rounded-lg'onChange={handlechange}/>
         <input type="password" placeholder='password' id='password'className='p-3 rounded-lg' onChange={handlechange}/>
-        <button className='p-3 rounded-lg uppercase bg-slate-800 text-white hover:opacity-90 disabled:opacity-80'>sign up</button>
+        <button className='p-3 rounded-lg uppercase bg-slate-800 text-white hover:opacity-90 disabled:opacity-80' onClick={setLoading}> {loading? 'Loading...' : 'signup'} </button>
       </form>
       <div className='flex gap-2 mt-4'>
         <p>Have an account?</p>
         <Link to='/signin'>
         <span className='text-blue-800'>Log In</span>
         </Link>
-      </div>
+      </div> 
+      <p className='text-red-700 mt-5'>{error && 'something went  wrong!'}</p>
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+
   plugins: [react()],
-})
+
+  server:{
+    proxy:{
+      '/api':{
+        target: 'http://127.0.0.1:3030',
+        secure:false,
+      },
+    },
+  },
+
+});
