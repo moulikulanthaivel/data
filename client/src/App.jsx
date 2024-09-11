af0fccb5a0db9a5a6c00597419b6708333e2e554
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-09-12 00:13:01 +0530

    tst

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
-import PrivateRouter from './components/privateRouter'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -15,12 +15,12 @@ export default function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
 
-        <Route element={<PrivateRouter/>}>
+        <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile/>}/>
         </Route>
 
         <Route path='/signin' element={<SignIn/>}/>
-        
+
         <Route path='/signup' element={<SignUp/>}/>
       </Routes>
     </BrowserRouter>
