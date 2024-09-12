0616c5972c7d71b5540747a0a74e64ea1623fb70
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-09-12 19:55:35 +0530

    tst

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,38 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import {getStorage, ref, uploadBytesResumable} from "firebase/storage"
+import { app } from "../firebase";
 
 export default function Profile() {
   const {currentUser} = useSelector(state=>state.user) 
+  const fileRef = useRef(null);
+  const [image , setImage] = useState(undefined);
+
+ useEffect(()=>{
+  if(image){
+    handleFileUpload(image);
+  }
+ })
+
+ const handleFileUpload= async(image)=>{
+  const storage = getStorage(app);
+  const fileName = new Date().getTime + image.name;
+  const storageRef = ref(storage , fileName);
+  const uploadTask = uploadBytesResumable(storageRef , image)
+  uploadTask.on(
+    'state_changed',
+    (snapshot)=>{
+      const progess = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('upload is' + progess + '% done')
+    }
+  )
+ }
   return (
     <div className="max-w-lg mx-auto">
     <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     <form className="flex flex-col gap-4">
-        <img src={currentUser.profilePicture} alt="picture"className="h-24 w-24 self-center cursor-pointer rounded-full object-cover" />
+      <input type="file" ref={fileRef} hidden accept="image/*" onChange={(e)=>{setImage(e.target.files)}} />
+        <img src={currentUser.profilePicture} alt="picture"className="h-24 w-24 self-center cursor-pointer rounded-full object-cover" onClick={()=>{fileRef.current.click()}} />
       <input defaultValue={currentUser.username} type="text"  id="username" placeholder="username" className=" rounded-lg bg-slate-100 p-3 mt-2"  />
       <input defaultValue={currentUser.email} type="email" id="email" placeholder="email" className=" rounded-lg bg-slate-100 p-3 mt-2"  />
       <input type="text"  id="password" placeholder="password" className=" rounded-lg bg-slate-100 p-3 mt-2"  />
